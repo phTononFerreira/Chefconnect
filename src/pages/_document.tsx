04f41337c92a0dbdc1f05e0fe37da9f2f3d77c70
phTononFerreira <pehetofe@gmail.com>
2023-02-28 12:11:31 -0300

    🚔 Content-Security-Policy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document(){
     return(
         <Html>
             <Head>
-
+                <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
             </Head>
             <body>
                 <Main/>
